64d2abcd6d944772d2f6399d6f59d9716d8982e2
Vojtech Hordejcuk <vojtech.hordejcuk@gmail.com>
2015-03-24 01:08:04 +0100

    simple url encoding

--- app/src/main/java/service/EncodingService.java
+++ app/src/main/java/service/EncodingService.java
@@ -1,9 +1,7 @@
 package service;
 
-import net.sourceforge.plantuml.code.Base64Coder;
-
 public class EncodingService {
     public String decode(String dataIn) {
-        return Base64Coder.decodeString(dataIn);
+        return dataIn;
     }
 }
---
+++ app/src/test/java/service/EncodingServiceTest.java
@@ -0,0 +1,14 @@
+package service;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class EncodingServiceTest {
+    private EncodingService service = new EncodingService();
+
+    @Test
+    public void testDecode() throws Exception {
+        // TODO
+    }
+}
